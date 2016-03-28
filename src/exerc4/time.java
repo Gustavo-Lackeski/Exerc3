15478e3df24549396a5ba27d3c72eb78e8854444
Gustavo-Lackeski <glackeski@gmail.com>
2016-03-28 00:49:18 -0300

    Exerc3

---
+++ src/exerc4/time.java
@@ -0,0 +1,23 @@
+
+package exerc4;
+public class time {
+    int hour;
+    int minute;
+    time (){} ;
+    void setH(int x){
+        hour = x;
+    }
+    void setM(int x){
+        minute = x;
+    }
+    void writetime(){
+        System.out.println("The time now is " +hour+":"+minute);    
+        
+    }
+    public static void main(String[] args) {
+        time t = new time();
+        t.setH(3);
+        t.setM(25);
+        t.writetime();
+    }    
+}
